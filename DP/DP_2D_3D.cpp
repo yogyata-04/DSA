5483302bf0bfeb004d984512232900c703d3fff0
Yogyata Singh <134354066+yogyata-04@users.noreply.github.com>
2025-02-10 18:29:55 +0530

    Update and rename DP_2D.cpp to DP_2D_3D.cpp

--- DP/DP_2D_3D.cpp
+++ DP/DP_2D_3D.cpp
@@ -165,6 +165,138 @@ int minPathSum(vector<vector<int>>& obstacleGrid) {
     return dp[m-1][n-1];
 }
 
+
+//minimum path sum in a triangular grid
+//dp solution - TC->O((n*(n-1)/2)*2) SC->O(n^2)
+int solve(int row,int col,vector<vector<int>> triangle,vector<vector<int>> &dp,int n){
+    if(col>row || row>n) return INT_MAX;
+    if(row==n) {
+        return 0;
+    }
+    if(dp[row][col]!=-1) return dp[row][col];
+    //starting from top
+    //move downright
+    int downright=INT_MAX;
+    if(col<=row) downright=solve(row+1,col+1,triangle,dp,n);
+    //move dowm
+    //take care here we will not have right instead we will have down as specified in question
+    int down=solve(row+1,col,triangle,dp,n);
+
+    return dp[row][col]=min(downright,down)+triangle[row][col];
+}
+int minimumTotal(vector<vector<int>>& triangle) {
+    int n=triangle.size();
+    vector<vector<int>> dp(n,vector<int> (n,-1));
+    //we just want to reach last row
+    int temp=solve(0,0,triangle,dp,n);
+    //here remember we are returning temp;
+    return temp;
+}
+
+//can also do it like
+int solve(int row,int col,vector<vector<int>> triangle,vector<vector<int>> &dp,int n){
+    if(col>row || row>n) return INT_MAX;
+    if(row==n-1) {
+        return triangle[row][col];
+    }
+    if(dp[row][col]!=-1) return dp[row][col];
+    //starting from top
+    //move downright
+    int downright=INT_MAX;
+    if(col<=row) downright=solve(row+1,col+1,triangle,dp,n);
+    //move dowm
+    //take care here we will not have right instead we will have down as specified in question
+    int down=solve(row+1,col,triangle,dp,n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    return dp[row][col]=min(downright,down)+triangle[row][col];
+}
+
+
+
+int minimumTotal(vector<vector<int>>& triangle) {
+    int n=triangle.size();
+    vector<vector<int>> dp(n,vector<int> (n,-1));
+    //we just want to reach last row
+    int temp=solve(0,0,triangle,dp,n);
+    return temp;
+}
+
+//Minimum falling path sum
+//DP approach TC->O(N^2*3) SC->O(N^2+O(N)(recursion stack space))
+int solve(int row,int col,vector<vector<int>> &matrix,int n,vector<vector<int>> &dp){
+    if(row>=n || col<0 || col>=n) return INT_MAX;
+    if(row==n-1){
+        return matrix[row][col];
+    }
+    if(dp[row][col]!=-1) return dp[row][col];
+    //move down
+    int down=solve(row+1,col,matrix,n,dp);
+    //move downleft
+    int downleft=solve(row+1,col-1,matrix,n,dp);
+    //move downright
+    int downright=solve(row+1,col+1,matrix,n,dp);
+    return dp[row][col]=min(down,min(downleft,downright)) + matrix[row][col];
+}
+int minFallingPathSum(vector<vector<int>>& matrix) {
+    int n=matrix.size();
+    vector<vector<int>> dp(n,vector<int>(n,-1));
+    int ans=INT_MAX;
+    for(int i=0;i<n;i++){
+        ans=min(ans,solve(0,i,matrix,n,dp));
+    }
+    return ans;
+}
+
+//3D dp
+//Alice and bob problem for maximum sum Alice(0,0) Bob(0,m-1)
+//we can work with even one row instead of rowa and rowb
+//without memoisation TC->O(3^n*3^n) SC->O(n)(recursion stack space)
+//After memoization TC->O(N*M*M*9) SC->O(N*M*M)
+int solve(int rowa,int cola,int rowb,int colb,vector<vector<int>> grid,int n,int m,vector<vector<vector<int>>> &dp){
+    if(rowa>=n || rowb>=n || cola<0 || cola>=m || colb<0 || colb>=m) return INT_MIN;
+    if(rowa==n-1 && rowb==n-1){
+        if(cola==colb){
+            return grid[rowa][cola];
+        }
+        return grid[rowa][cola]+grid[rowb][colb];
+    }
+    if(dp[rowa][cola][colb]!=-1) return dp[rowa][cola][colb];
+    //storing movements column wise , now need of row as it will always increment by 1
+    vector<int> dy={-1,0,1};
+    int maxi=INT_MIN;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(cola==colb){
+                maxi=max(maxi,grid[rowa][cola]+solve(rowa+1,cola+dy[i],rowb+1,colb+dy[j],grid,n,m,dp));
+            }
+            else{
+                maxi=max(maxi,grid[rowa][cola]+grid[rowb][colb]+solve(rowa+1,cola+dy[i],rowb+1,colb+dy[j],grid,n,m,dp));
+            }
+        }
+    }
+    return dp[rowa][cola][colb]=maxi;
+}
+
+int alicebobmax(vector<vector<int>> grid){
+    int n=grid.size();
+    int m=grid[0].size();
+    //row*alicecol*bobcol n*m*m
+    vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
+    int temp=solve(0,0,0,m-1,grid,n,m,dp);
+    return temp;
+}
+
+int main(){
+    vector<vector<int>> vec={{2,3,1,2},{3,4,2,2},{5,6,3,5}};
+    cout<<alicebobmax(vec);
+}
+
 int main(){
     
 }
