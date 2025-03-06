25bcc85b3e499caaa428d6887c4f3fbc665579de
Yogyata Singh <134354066+yogyata-04@users.noreply.github.com>
2025-03-06 15:17:03 +0530

    Create removal_game.cpp

---
+++ cses/DP/removal_game.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//this is a good question
+//initially I approached it like player 1 can move anyways and player2 will choose maximum element
+//which is wrong player2 will minimize player1 score instead of maximising it's own
+int solve(int i,int j,vector<int> vec,vector<vector<int>> &dp){
+    if(i>j){
+        return 0;
+    }
+    if(dp[i][j]!=-1) return dp[i][j];
+    // Player 1 picks `i` or `j` and Player 2 plays optimally to minimize P1's future score
+    int pick_left=vec[i]+min(solve(i+2,j,vec,dp),solve(i+1,j-1,vec,dp));
+    int pick_right=vec[j]+min(solve(i,j-2,vec,dp),solve(i+1,j-1,vec,dp));
+    return dp[i][j]=max(pick_left,pick_right);
+}
+
+int main(){
+   int n;
+   cin>>n;
+   vector<int> vec(n);
+   for(int i=0;i<n;i++){
+       cin>>vec[i];
+   }
+   vector<vector<int>> dp(n,vector<int>(n,-1));
+   cout<<solve(0,n-1,vec,dp);
+}
